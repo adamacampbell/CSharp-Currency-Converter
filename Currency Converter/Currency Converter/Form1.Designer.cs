4440d01a4c35738b86828ef0c130b82660551bf1
Adam Alexander Campbell <adamalexandercampbell@gmail.com>
2020-10-29 14:26:42 +0000

    Implemented basic functionality
    Implemented:
    
    * Basic functionality allowing users to enter a given value and select a currency to convert to.
    
    Todo:
    
    * Update original value what converted value is changed.
    * Allow converstion between other currencies, rather than just GBP to other currencies.
    * Update currency selection list to display full currency name.
    * Update UI to display given exchange rate at all times.

---
+++ Currency Converter/Currency Converter/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Currency_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.originalValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.currencySelectionBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.convertedValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // originalValue
+            // 
+            this.originalValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.originalValue.Location = new System.Drawing.Point(12, 25);
+            this.originalValue.Name = "originalValue";
+            this.originalValue.Size = new System.Drawing.Size(776, 20);
+            this.originalValue.TabIndex = 0;
+            this.originalValue.TextChanged += new System.EventHandler(this.originalValue_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Value in Pounds";
+            // 
+            // currencySelectionBox
+            // 
+            this.currencySelectionBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.currencySelectionBox.FormattingEnabled = true;
+            this.currencySelectionBox.Location = new System.Drawing.Point(12, 64);
+            this.currencySelectionBox.Name = "currencySelectionBox";
+            this.currencySelectionBox.Size = new System.Drawing.Size(776, 21);
+            this.currencySelectionBox.TabIndex = 2;
+            this.currencySelectionBox.SelectedIndexChanged += new System.EventHandler(this.currencySelectionBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Convert To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Value";
+            // 
+            // convertedValue
+            // 
+            this.convertedValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.convertedValue.Location = new System.Drawing.Point(12, 104);
+            this.convertedValue.Name = "convertedValue";
+            this.convertedValue.Size = new System.Drawing.Size(776, 20);
+            this.convertedValue.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 137);
+            this.Controls.Add(this.convertedValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.currencySelectionBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.originalValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox originalValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox currencySelectionBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox convertedValue;
+    }
+}
+
---
+++ Currency Converter/Currency Converter/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+// Use post request
+using System.Net;
+using Newtonsoft.Json.Linq;
+// Use JSON
+
+
+namespace Currency_Converter
+{
+    public partial class Form1 : Form
+    {
+        // Global response val
+        JObject data = new JObject() { };
+        // Global exchange rate val
+        double exRate = 0;
+        // Global input val
+        double input = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Get currency list
+            // Create a request for the URL
+            WebRequest request = WebRequest.Create(
+                "https://v6.exchangerate-api.com/v6/23dc9198250b9ff553601f13/latest/GBP");
+            // If required by the server, set the credentials
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response
+            WebResponse response = request.GetResponse();
+            // Display the status
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            // Get the stream containing content returned by the server
+            // The using statement ensures that stream is closed outside of scope
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content
+                Console.WriteLine(responseFromServer);
+                // Set currency lists
+                this.data = JObject.Parse(responseFromServer);
+                Console.WriteLine(this.data);
+                Console.WriteLine("--------");
+                Console.WriteLine(this.data["conversion_rates"]);
+                // Update currency selection box
+                UpdateCurrencySelection();
+            }
+            // Close the response
+            response.Close();
+        }
+
+        private void UpdateCurrencySelection()
+        {
+                // Temp value to hold currencies
+                List<String> currencies = new List<string>();
+                // Loop through each currency
+                foreach (JProperty value in this.data["conversion_rates"])
+                {
+                    currencies.Add(value.Name);
+                }
+                // Update currency dropdown list values
+                currencySelectionBox.DataSource = currencies;
+        }
+
+        private void currencySelectionBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get currency exchaneg rate
+            string exRate = this.data["conversion_rates"][currencySelectionBox.SelectedItem].ToString();
+            Console.WriteLine(exRate);
+            Console.WriteLine("---------");
+            // Convert exchange rate from string to double
+            this.exRate = double.Parse(exRate);
+            Console.WriteLine(this.exRate);
+            Console.WriteLine("---------");
+            // Update output
+            updateOutput();
+        }
+        private void originalValue_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get original value as double
+                double val = double.Parse(originalValue.Text);
+                Console.WriteLine(val);
+                Console.WriteLine("---------");
+                // Update input val
+                this.input = val;
+                // Update output
+                updateOutput();
+            }
+            catch (FormatException err)
+            {
+                Console.WriteLine(err);
+            }
+        }
+
+        private void updateOutput()
+        {
+            try
+            {
+                // Calculate new value
+                double newVal = this.input * this.exRate;
+                Console.WriteLine(newVal);
+                Console.WriteLine("---------");
+                // Update output textbox value
+                convertedValue.Text = newVal.ToString();
+            } catch (FormatException err)
+            {
+                Console.WriteLine(err);
+            }
+        }
+
+    }
+}
